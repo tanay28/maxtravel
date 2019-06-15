a4d3868540251f09f94dda03c983de60cb2e05c9
tanay <tmtanay56@gmail.com>
2019-06-15 13:36:31 +0530

    mail opened

--- application/controllers/Contactus.php
+++ application/controllers/Contactus.php
@@ -25,21 +25,21 @@
 			$this->load->model('Contact_us');
 			$contactemail = $this->Contact_us->get_email_contact();
 			
-			// if($contactemail != '') $from_email = $contactemail;
-			// $this->load->library('email');
-			// $from_email = "tmtanay56@gmail.com";
-			// $to_email = "tmtanay56@gmail.com"; // this email for testing purpose
-			// $this->email->from($from_email, 'Tanay'); 
- 		// 	$this->email->to($to_email);
- 		// 	$this->email->subject('New User Contact Details'); 
- 		// 	//$this->email->set_mailtype("html");
- 		// 	$this->email->set_header('Content-type', 'text/html');
- 		// 	$this->load->library('custom_email');
- 		// 	$msg_mail = $this->custom_email->contact_info($name,$email,$phone,$msg);
- 		// 	$this->email->message($msg_mail);
- 		// 	$chkk = $this->email->send();
+			if($contactemail != '') $from_email = $contactemail;
+			$this->load->library('email');
+			$from_email = "tmtanay56@gmail.com";
+			$to_email = "tmtanay56@gmail.com"; // this email for testing purpose
+			$this->email->from($from_email, 'Tanay'); 
+ 			$this->email->to($to_email);
+ 			$this->email->subject('New User Contact Details'); 
+ 			//$this->email->set_mailtype("html");
+ 			$this->email->set_header('Content-type', 'text/html');
+ 			$this->load->library('custom_email');
+ 			$msg_mail = $this->custom_email->contact_info($name,$email,$phone,$msg);
+ 			$this->email->message($msg_mail);
+ 			$chkk = $this->email->send();
 
- 			//if(!$chkk) $status = 'FAILED';
+ 			if(!$chkk) $status = 'FAILED';
 			//------------------------- END ---------------------//
 			$arr = array(
 				'name'        => $name,
