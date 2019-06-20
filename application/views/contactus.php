823f7b5c853ea25cc30400f31fe991d8e63ff0de
Tanay <tmtanay56@gmail.com>
2019-06-20 23:13:05 +0530

    common footer added in inner page contactus,joinus,aboutus

--- application/views/contactus.php
+++ application/views/contactus.php
@@ -161,43 +161,4 @@
 					</div>
 				</div>
 			</footer> -->
-			<?php include_once('footer_box.php');?>
-
-		</div>
-	</div>
-	<!-- jQuery -->
-	<script src="<?php echo base_url('assets/js/jquery.min.js');?>"></script>
-	<script>
-		$(window).load(function () {
-			jQuery('#page').fadeIn(500);
-			jQuery('#loading').hide();
-		});
-	</script>
-	<!-- jQuery Easing -->
-	<script src="<?php echo base_url('assets/js/jquery.easing.1.3.js');?>"></script>
-	<!-- Bootstrap -->
-	<script src="<?php echo base_url('assets/js/bootstrap.min.js');?>"></script>
-	<!-- Owl Carousel -->
-	<!-- Waypoints -->
-	<script src="<?php echo base_url('assets/js/jquery.waypoints.min.js');?>"></script>
-
-	<script src="<?php echo base_url('assets/js/owl.carousel.min.js');?>"></script>
-	<!-- Main JS (Do not remove) -->
-	<script src="<?php echo base_url('assets/js/main.js');?>"></script>
-	<script src="<?php echo base_url('assets/js/jquery.nice-select.min.js');?>"></script>
-	<script>
-		$(document).ready(function () {
-			$('select:not(.ignore)').niceSelect();
-			FastClick.attach(document.body);
-		});    
-	</script>
-	<script>
-		$('.sub-menu ul').hide();
-		$(".sub-menu a").click(function () {
-			$(this).parent(".sub-menu").children("ul").slideToggle("100");
-			$(this).find(".right").toggleClass("fa-caret-up fa-caret-down");
-		});
-	</script>
-</body>
-
-</html>
+			<?php include_once('footer.php');?>
