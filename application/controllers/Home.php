c1682d7cd5aefaf4ca4726df8173a5c882145aeb
Tanay <tmtanay56@gmail.com>
2019-06-20 22:19:55 +0530

    timezone function added. removed timezone api

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,6 +1,6 @@
 <?php
 	defined('BASEPATH') OR exit('No direct script access allowed');
-	//date_default_timezone_set("Asia/Kolkata");
+	date_default_timezone_set("Asia/Kolkata");
 	class Home extends CI_Controller
 	{
 		function __construct()
@@ -20,16 +20,8 @@
 
 		private function get_local_time($timezone)
 		{
-			$url = "http://worldtimeapi.org/api/timezone/".$timezone;
-			$curl_handle=curl_init();
-			curl_setopt($curl_handle,CURLOPT_URL,$url);
-			curl_setopt($curl_handle,CURLOPT_CONNECTTIMEOUT,2);
-			curl_setopt($curl_handle,CURLOPT_RETURNTRANSFER,1);
-			$buffer = curl_exec($curl_handle);
-			curl_close($curl_handle);
-			$arr = json_decode($buffer,true);
-			$curr = date('H:i:s',strtotime($arr['datetime']));
-			return $curr;
+			date_default_timezone_set($timezone);
+			return date('H:i:s');
 		}
 	}
 
